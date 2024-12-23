c880637f43e956a72e1f23bb64c6ba9e49ed16c6
jamone- <dominik.rabij@gmail.com>
2024-12-23 10:28:48 +0100

    23: Bron-Kerbosch

--- src/main/kotlin/23.kt
+++ src/main/kotlin/23.kt
@@ -5,22 +5,12 @@ fun main() {
 
 fun solve23a(lines: List<String>): Int {
     val map = parseInput(lines)
-    return generateParties(map, size = 3).filter { p -> p.used.any { it.startsWith("t") }}.size
+    return generateParties(map, size = 3).filter { p -> p.used.any { it.startsWith("t") } }.size
 }
 
 fun solve23b(lines: List<String>): String {
     val map = parseInput(lines)
-
-    var parties: Set<Party> = setOf(Party(used = setOf(), unused = map.keys))
-    var lastParties: Set<Party> = parties
-
-    while (parties.isNotEmpty()) {
-        lastParties = parties
-        parties = expandParties(map, lastParties)
-    }
-
-    assert(lastParties.size == 1)
-    return lastParties.first().used.sorted().joinToString(",")
+    return bronKerbosch(map).used.sorted().joinToString(",")
 }
 
 private fun parseInput(lines: List<String>): Map<String, Set<String>> {
@@ -60,3 +50,24 @@ private fun isConnectedWithAllFrom(map: Map<String, Set<String>>, x: String, par
     party.all { it in map[x]!! }
 
 private data class Party(val used: Set<String>, val unused: Set<String>)
+
+private fun bronKerbosch(map: Map<String, Set<String>>): Party {
+    fun aux(r: Set<String>, pIn: Set<String>): Set<String>? {
+        if (pIn.isEmpty()) {
+            return r
+        }
+
+        var p = pIn
+        val candidates = mutableListOf<Set<String>?>()
+        for (v in p) {
+            val result = aux(r + v, p.intersect(map[v]!!))
+            candidates.add(result)
+            p -= v
+        }
+
+        return candidates.filterNotNull().maxByOrNull { it.size }
+    }
+
+    val result = checkNotNull(aux(setOf(), map.keys))
+    return Party(used = result, unused = map.keys - result)
+}
